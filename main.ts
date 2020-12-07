159fa598c073b7f6180429ee6c78778bf83cb81c
Alex Ligkas <ligkalex@gmail.com>
2020-12-08 00:57:57 +0200

    Install eslint

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const server = fastify();
 
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
+server.get('/ping', async (request, reply) => {
+  return 'pong\n';
 });
 
 server.listen(8080, (err, address) => {
