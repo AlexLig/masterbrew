ab7c87a82aac82dc56b8fcf959241446a9a634d6
Alex Ligkas <ligkalex@gmail.com>
2020-12-08 03:18:37 +0200

    install express

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify';
+import express from 'express';
 
 const server = fastify();
 
-server.get('/ping', async (request, reply) => {
+server.get('/ping', async () => {
   return 'pong\n';
 });
+
 type LoginQuery = {
   username: string;
   password: string;
@@ -22,12 +24,12 @@ server.get<{
     preValidation: (request, reply, done) => {
       const { username, password } = request.query;
       // only validate `admin` account
-      const error = username !== 'admin' ? new Error('Must be admin dude rly') : undefined;
-      console.log(error)
+      const error =
+        username !== 'admin' ? new Error('Must be admin dude rly') : undefined;
       done(error);
     }
   },
-  async (request, reply) => {
+  async (request) => {
     const { username, password } = request.query;
     const customerHeader = request.headers['H-Custom'];
     // do something with request data
@@ -36,10 +38,20 @@ server.get<{
   }
 );
 
-server.listen(8080, (err, address) => {
+server.listen(8080, (err) => {
   if (err) {
-    console.error(err);
     process.exit(1);
   }
-  console.log(`Server listening at ${address}`);
+});
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
